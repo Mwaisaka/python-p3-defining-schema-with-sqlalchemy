f57be2b08d8a6a493ba0757d5a85cf2e84140207
Frank <frankkashere@gmail.com>
2024-02-12 19:56:52 +0300

    Updated code

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -6,7 +6,19 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class Student(Base):
-    pass
-
+    __tablename__ = 'students'
+    
+    id=Column(Integer,primary_key=True)
+    name=Column(String())
+    age=Column(String())
+    
+    # __tablename__ = 'pets'
+    
+    # id=Column(Integer,primary_key=True)
+    # name=Column(String())
+    # age=Column(String())
+    
 if __name__ == '__main__':
-    pass
+    engine = create_engine('sqlite:///students.db')
+    Base.metadata.create_all(engine)
+    
